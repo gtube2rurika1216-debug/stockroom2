b935e955f45bb4ad9074a3e52eb691a0f18da40d
gtube2rurika1216-debug <gtube2rurika1216@gmail.com>
2026-08-24 19:07:27 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.STOCKROOM_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyBtx_EUkzpl7AlEyGKQWb13JsAElNXNnR8',
+  authDomain: 'stockroom2-43ad2.firebaseapp.com',
+  projectId: 'stockroom2-43ad2',
+  storageBucket: 'stockroom2-43ad2.firebasestorage.app',
+  messagingSenderId: '854349036459',
+  appId: '1:854349036459:web:a3b16d7cc01975aa20b0ee'
 };
